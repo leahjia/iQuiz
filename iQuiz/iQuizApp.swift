a34551e1bb80e53814c1b573e1ed3e1ecd455102
Leah Jia <99011487+leahjia@users.noreply.github.com>
2024-05-08 23:54:38 -0700

    Initial Commit

---
+++ iQuiz/iQuizApp.swift
@@ -0,0 +1,17 @@
+//
+//  iQuizApp.swift
+//  iQuiz
+//
+//  Created by Leah on 5/8/24.
+//
+
+import SwiftUI
+
+@main
+struct iQuizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
